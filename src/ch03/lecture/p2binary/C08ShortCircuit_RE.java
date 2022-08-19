8213eeff1f15229d4c179dc9bf26f8ca6209ed0f
Ahnseo <ahnseoj@gmail.com>
2022-08-19 18:03:27 +0900

    shortCircuit_Review

--- src/ch03/lecture/p2binary/C08ShortCircuit_RE.java
+++ src/ch03/lecture/p2binary/C08ShortCircuit_RE.java
@@ -2,22 +2,33 @@ package ch03.lecture.p2binary;
 
 public class C08ShortCircuit_RE {
 	public static void main(String[] args) {
-		//ShortCircuit : && 와 || 에서 발생함   ( &, | 에서는 발생 안함 )
+		// short circuit
+		// &&, || => 발생
+		
+		//  &는 두 조건을 무조건 검사하는것이고, 
+		// &&는 첫번째 조건이 참일때만 검사하는것이다  
+		// | 와 || 도 동일하다.
+		
 		int i = 3;
 		
-		if ( i<0 && i >-5) {  //i<0 에서 false 로 바로 다음 코드로 넘어가서 숏트 남 
-			System.out.println("i는 -5보다 크고 0보다 작다");
-		}
-		if ( (i++) < 0 && (i++) >-5) {  //i<0 에서 false 로 바로 다음 코드로 넘어가서 숏트 남 
+		if (i < 0 && i > -5) { //i < 0 에서 false 이니까 건너뜀.(숏트)
 			System.out.println("i는 -5보다 크고 0보다 작다");
 		}
 		
+		if ((i++) < 0 && (i++) > -5) { //(i++) < 0 에서 false이니까 건너뜀 (숏트)
+			System.out.println("i는 -5보다 크고 0보다 작다");
+		}
 		
-		// &, | 에서는 발생 안함
+		System.out.println("i:" + i); // (i++) < 0 이후 i = 4 가되었다.
 		
-		int j = 4;
-		if ( ((j++) < 0) & ((j++) > -5) ) {  
-			System.out.println("j:" + j);
-		}	
+		
+		// &, | => 발생안함  &, | 는 두 조건을 무조건 검사하기 때문이다.
+		int k = 3;
+		if ((k++) < 0 & (k++) > -5) {
+			System.out.println("j는 -5보다 크고 0보다 작다");
+		}
+		System.out.println("k:" + k);
+		
+		System.out.println("프로그램 종료");
 	}
 }
